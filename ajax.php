f21753630bfd946d5a7866db21a267b44029c75a
bernimoses <bernimoses@dnsproject.de>
2012-02-19 15:29:47 +0100

    Initial import, with final looking interface for request list and new request.

---
+++ ajax.php
@@ -0,0 +1,14 @@
+<?php
+
+echo json_encode(array(
+	'requests' => array(
+		array(
+			'title' => 'Lorem Ipsum',
+			'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut tortor ante. Cras posuere, urna ut vulputate pretium.',
+			'name' => 'Someone famous',
+			'votes_count' => '12345'
+		)
+	)
+));
+
+?>
---
+++ js/requestbox.js
@@ -0,0 +1,37 @@
+
+(function($) {
+	$(document).ready(function() {
+		var itemTemplate = $('#item-template');
+
+		var newItem = $('#new-item');
+		var newItemLink = $('a[href="#new-item"]');
+
+		$.ajax({
+			url: '/ajax.php',
+			dataType: 'json',
+			success: function(object) {
+				$.each(object.requests, function() {
+					$(itemTemplate).tmpl(this).appendTo('.request-list');
+				});
+			}
+		});
+
+		newItem.on('show', function() {
+			$('.new-item', newItemLink).hide();
+			$('.close-form', newItemLink).show();
+		}).on('hide', function() {
+			$('.new-item', newItemLink).show();
+			$('.close-form', newItemLink).hide();
+		}).collapse('hide').show();
+
+		$('button[type="reset"]', newItem).click(function() {
+			newItemLink.click();
+		});
+
+		$('button[type="submit"]', newItem).click(function() {
+			alert('submit');
+
+			return false;
+		});
+	});
+})(jQuery);
